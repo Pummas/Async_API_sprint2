f3e891bc59077a3c679a706702a8d1cc23dad289
Pumas <l.a.r.p@icloud.com>
2022-06-28 12:06:40 +0400

    Вернул версию редиса

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# import aioredis
+import aioredis
 import uvicorn
 from elasticsearch import AsyncElasticsearch
 from fastapi.applications import FastAPI
@@ -18,9 +18,9 @@ app = FastAPI(
 
 @app.on_event('startup')
 async def startup():
-    # redis.redis = await aioredis.create_redis_pool(
-    # (config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20
-    # )
+    redis.redis = await aioredis.create_redis_pool(
+        (config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20
+    )
     elastic.es = AsyncElasticsearch(
         hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}']
     )
