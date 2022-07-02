b8689fede5776bb969dfd19e4b806d2bc7bcc6f9
Pumas <l.a.r.p@icloud.com>
2022-07-03 00:48:54 +0400

    Тесты для Genres (Поиск всех жанров, поиск жанра по ключу, поиск с учетом кеширования)

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ async def shutdown():
 # Подключаем роутер к серверу, указав префикс /v1/films
 # Теги указываем для удобства навигации по документации
 app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
-app.include_router(genre.router, prefix='/api/v1/genre', tags=['genres'])
+app.include_router(genre.router, prefix='/api/v1/genres', tags=['genres'])
 app.include_router(persons.router, prefix='/api/v1/persons', tags=['persons'])
 
 if __name__ == '__main__':
--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -5,13 +5,17 @@ import aiohttp
 import redis
 
 from dataclasses import dataclass
+
+from elasticsearch import AsyncElasticsearch
 from multidict import CIMultiDictProxy
 
 # from src.core.config import REDIS_HOST, REDIS_PORT
 
+# from tests.functional.settings import TestSettings
+
 from .settings import TestSettings
 
-settings = TestSettings
+settings = TestSettings()
 
 
 @dataclass
@@ -21,9 +25,9 @@ class HTTPResponse:
     status: int
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def make_get_request(session):
-    async def inner(method: str, params: dict) -> HTTPResponse:
+    async def inner(method: str, params: dict = None) -> HTTPResponse:
         params = params or {}
         url = settings.base_api + method
         async with session.get(url, params=params) as response:
@@ -38,13 +42,13 @@ def make_get_request(session):
 
 @pytest.fixture(scope="session")
 def event_loop():
-    loop = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
 
 @pytest.fixture(scope="session")
-async def session():
+async def session(event_loop):
     session = aiohttp.ClientSession()
     yield session
     await session.close()
@@ -55,3 +59,10 @@ def redis_client():
     return redis.Redis(
         settings.redis_host, settings.redis_port
     )  # type: ignore
+
+
+@pytest.fixture(scope='session')
+async def es_client():
+    client = AsyncElasticsearch(hosts=f'127.0.0.1:9200')
+    yield client
+    await client.close()
